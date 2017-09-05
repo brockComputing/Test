c167d4cf0bc576ade0a15395663ae5622d416cfc
brockCompSci <cbrown@brock.ac.uk>
2017-09-05 13:17:34 +0100

    Add project files.

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age = -1;
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+            while (age >= 120 || age < 0)
+            {
+                Console.WriteLine("Enter your age");
+                age = Convert.ToInt32(Console.ReadLine()); 
+            }
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("you are " + age + " years old");
+            Console.WriteLine((age * 12) + " months old");
+            Console.WriteLine((age * 365) + " days old");
+            Console.WriteLine((age * 365 * 24) + " hours old");
+            Console.ReadLine();
+
+        }
+    }
+}
